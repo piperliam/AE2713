535ad67877f9d95d704216008b669d3a2e1605c9
Piperliam <68978670+piperliam@users.noreply.github.com>
2021-11-02 10:12:04 -0400

    Update plotl1q5.m

--- plotl1q5.m
+++ plotl1q5.m
@@ -5,7 +5,9 @@ y2=exp(-1.2*x).*sin(5.3*x) ;
 y3=exp(-2.1*x).*sin(2.5*x) ;
 figure(1)
 plot(x, y1)
-title("Wow this is a graph!")
+title("Figure 1 – Amplitude of a damped system.")
+xlabel('Time') 
+ylabel('Amplitude') 
 hold on
 plot(x, y2)
 plot(x, y3)
