445a5cabc32afd97219489f4760b1165b90f24e0
Piperliam <68978670+piperliam@users.noreply.github.com>
2021-11-02 10:05:36 -0400

    Add files via upload

---
+++ plotl1q5.m
@@ -0,0 +1,13 @@
+
+x = linspace(0, 5);
+y1=exp(-0.5*x).*sin(8*x) ; 
+y2=exp(-1.2*x).*sin(5.3*x) ;
+y3=exp(-2.1*x).*sin(2.5*x) ;
+figure(1)
+plot(x, y1)
+title("Wow this is a graph!")
+hold on
+plot(x, y2)
+plot(x, y3)
+hold off
+grid
